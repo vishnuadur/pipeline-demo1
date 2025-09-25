08886da60d3a1415de0b7555af9156b1a9b35144
vishnuadur <vishnu.adur2@gmail.com>
2025-09-25 14:42:16 +0000

    Correcting pipeline syntax and build script permissions

---
+++ build/CMakeFiles/myfirmware.elf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfirmware.elf.dir/src/main.c.o"
+  "CMakeFiles/myfirmware.elf.dir/src/main.c.o.d"
+  "myfirmware.elf"
+  "myfirmware.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myfirmware.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
